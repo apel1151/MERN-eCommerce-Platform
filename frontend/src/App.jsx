0411d5544e43e6d0cbc3575180fc0b0f0592d8b3
apel1151 <apelmahmud1151@gmail.com>
2023-12-01 19:18:50 +0800

    add routes and create pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+// App.jsx
+
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
