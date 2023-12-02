3657dae7bde2dcde091d19099e9a4d3508d73c28
apel1151 <apelmahmud1151@gmail.com>
2023-12-02 14:46:08 +0800

    db connected successfully

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,26 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 
+// creating server
 const app = express();
 
-app.listen(5000, () =>{
-    console.log("Server is litening on port 3000");
-})
+// connecting to the database
+
+// MongoDB connection
+mongoose
+  .connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected successfully");
+    // Start the server after successfully connecting to the database
+    app.listen(process.env.PORT, () => {
+      console.log("Port is listening at-", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to database:".red, error);
+  });
